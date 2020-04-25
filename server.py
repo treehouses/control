bd33d027130b7c34c599d9adbe01b4ac120f4661
lordjashin32 <tristanlangford32@gmail.com>
2020-04-25 00:13:02 +0000

    trial 1

--- server.py
+++ server.py
@@ -11,127 +11,34 @@ import threading
 import socket
 import string
 import random
-import os
-
-_closables = []
-
-
-def _SignalHandler(signum, frame):  # pylint: disable=unused-argument
-    print("\nCaught signal %d\n" % signum)
-    for closable in _closables:
-        closable.Close()
-
 
 def _ExceptionHandler(exc_type, exc_value, exc_traceback):
     sys.__excepthook__(exc_type, exc_value, exc_traceback)
     os.kill(os.getpid(), signal.SIGINT)
 
-
-class BluetoothServer(object):
-
-    def __init__(self):
-        self._lock = threading.Lock()
-        self._cond = threading.Condition(self._lock)
-        self._server_socket = None
-        self._client_socket = None
+#This is what gets spawned by the server when it receives a connection.
+class Worker(threading.Thread):
+    def __init__(self, sock, address):
+        threading.Thread.__init__(self)
+        self.sock = sock
+        self.address = address
         self._logger = logging.getLogger("logger")
-        self._adapter = dbus.Interface(dbus.SystemBus().get_object(
-            "org.bluez", "/org/bluez/hci0"), "org.freedesktop.DBus.Properties")
-        self._adapter.Set("org.bluez.Adapter1", "Powered", dbus.Boolean(1))
-        self._closed = False
-        self.set_host_name()
+        self.stopped = False
 
-    def __del__(self):
-        self.Close()
+    def send_msg(self, message):
+        self._logger.info("SendMessage: %s" % message)
+        self.sock.send(message)
 
-    def Close(self):
-        """Closes the server and all assiciated resources."""
-        with self._lock:
-            if self._closed:
-                return
-            self._closed = True
-            self._cond.notifyAll()
-
-        if self._client_socket is not None:
-            self._client_socket.close()
-
-        if self._server_socket is not None:
-            self._server_socket.close()
-
-        self.set_discoverable(False)
-
-    def run(self):
-        if self._closed:
-            return not self._closed
-
-        subprocess.call("/usr/bin/sdptool add SP", shell=True)
-
-        self._server_socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-        self._server_socket.bind(("", bluetooth.PORT_ANY))
-        self._server_socket.listen(1)
-        self.hci_config_command("piscan")
-
-        uuid = "00001101-0000-1000-8000-00805F9B34FB"
-        bluetooth.advertise_service(
-            self._server_socket,
-            "rpi-bluetooth-server",
-            service_id=uuid,
-            service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
-            profiles=[bluetooth.SERIAL_PORT_PROFILE])
-
-        while True:
-            try:
-                self._logger.info("Waiting for connections")
-                self.set_discoverable(True)
-                self._client_socket, client_info = self._server_socket.accept()
-                self._logger.info(
-                    "Connection from %s on channel %d",
-                    client_info[0],
-                    client_info[1])
-            except bluetooth.BluetoothError as error:
-                self._logger.info("Stop waiting for connections (%s)", error)
-                break
-
-            try:
-                self.set_discoverable(False)
-                self.handle_connection()
-            except (IOError, bluetooth.BluetoothError):
-                pass
-            finally:
-                self._client_socket.close()
-                self._client_socket = None
-                self._logger.info("Connection from %s on channel %d ended",
-                                  client_info[0], client_info[1])
-        self._logger.info("Server done")
-        self.set_discoverable(False)
-        return not self._closed
-
-    def set_discoverable(self, discoverable):
-        adapter = self._adapter
-        if discoverable:
-            adapter.Set(
-                "org.bluez.Adapter1",
-                "DiscoverableTimeout",
-                dbus.UInt32(0))
-            adapter.Set("org.bluez.Adapter1", "Discoverable", dbus.Boolean(1))
-            self.hci_config_command("leadv 3")
-            self._logger.info("Discoverable enabled")
-        else:
-            adapter.Set("org.bluez.Adapter1", "Discoverable", dbus.Boolean(0))
-            self.hci_config_command("noleadv")
-            self._logger.info("Discoverable disabled")
-
-    def hci_config_command(self, command):
-        subprocess.call("/bin/hciconfig hci0 %s" % command, shell=True)
-
-    def handle_connection(self):
-        while True:
-            received_msg = self.get_msg()
-            self._logger.info("Received request '%s'" % received_msg)
-            self.handle_request(received_msg)
+    def get_msg(self):
+        data = str(self.sock.recv(1024).decode("utf-8"))
+        if len(data) == 0:
+            self.stopped = True
+        self._logger.info("received data from device %s on %s:  [%s]" % (self.address[0], threading.current_thread().getName(), data))
+        return data
 
     def handle_request(self, msg):
         try:
+            #self.send_msg("::start::")
             result = subprocess.check_output(msg, shell=True).decode('utf-8').strip()
             if not len(result):
                 self.send_msg("the command '%s' returns nothing " % msg)
@@ -139,16 +46,32 @@ class BluetoothServer(object):
                 self.send_msg(line + " ")
         except:
             self.send_msg("Error when trying to run the command '%s' " % msg)
+        #finally:
+            #self.send_msg("::end::")
 
-    def send_msg(self, message):
-        if self._client_socket is None:
-            return
-        self._logger.info("SendMessage: %s" % message)
-        self._client_socket.send(message)
+    def run(self):
+        self._logger.info("worker %s has started." % threading.current_thread().getName())
+        try:
+            while True:
+                self.handle_request(self.get_msg())
+                if self.stopped:
+                    break
+        except Exception as e:
+            pass
+        self.sock.close()
 
-    def get_msg(self):
-        data = self._client_socket.recv(1024).decode("utf-8")
-        return str(data)
+class Server():
+    def __init__(self):
+        self.q_len = 3
+        self.port = bluetooth.PORT_ANY
+        self.server_sock = None
+        self.name = "rpi-bluetooth-server"
+        self.uuid = "00001101-0000-1000-8000-00805F9B34FB"
+        self._logger = logging.getLogger("logger")
+        self._adapter = dbus.Interface(dbus.SystemBus().get_object(
+            "org.bluez", "/org/bluez/hci0"), "org.freedesktop.DBus.Properties")
+        self._adapter.Set("org.bluez.Adapter1", "Powered", dbus.Boolean(1))
+        self.set_host_name()
 
     def set_host_name(self):
         if not os.path.exists('/etc/bluetooth-id'):
@@ -166,17 +89,72 @@ class BluetoothServer(object):
         self._logger.info("Setting device name: '%s'", bt_name)
         self._adapter.Set("org.bluez.Adapter1", "Alias", dbus.String(bt_name))
 
+    def hci_config_command(self, command):
+        subprocess.call("/bin/hciconfig hci0 %s" % command, shell=True)
+
+    def start_server(self):
+        self.hci_config_command("piscan")
+        self.server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        self.server_sock.bind(("", self.port))
+        self.server_sock.listen(self.q_len)  #Queue up as many as 5 connect requests.
+        self._logger.info("listening on port %d" % self.port)
+
+    def advertise_service(self):
+        bluetooth.advertise_service(
+            self.server_sock,
+            self.name,
+            service_id=self.uuid,
+            service_classes=[self.uuid, bluetooth.SERIAL_PORT_CLASS],
+            profiles=[bluetooth.SERIAL_PORT_PROFILE])
+
+    def accept_connections(self):
+        while True:
+            self._logger.info("Waiting for connections")
+            client_sock, address = self.server_sock.accept()
+            self._logger.info("accepted connection from %s" % address[0])
+            Worker(client_sock, address).start()  #Spawns the worker thread.
+            self._logger.info("number of current threads is %s" % threading.active_count())
+            for thread in threading.enumerate():
+                self._logger.info(thread.getName())
+
+    def set_discoverable(self, discoverable):
+        adapter = self._adapter
+        if discoverable:
+            adapter.Set(
+                "org.bluez.Adapter1",
+                "DiscoverableTimeout",
+                dbus.UInt32(0))
+            adapter.Set("org.bluez.Adapter1", "Discoverable", dbus.Boolean(1))
+            self.hci_config_command("leadv 3")
+            self._logger.info("Discoverable enabled")
+        else:
+            adapter.Set("org.bluez.Adapter1", "Discoverable", dbus.Boolean(0))
+            self.hci_config_command("noleadv")
+            self._logger.info("Discoverable disabled")
+
+    def run(self):
+        self.set_discoverable(True)
+        self.start_server()
+        self.advertise_service()
+        self.accept_connections()
+        self.set_discoverable(False)
+
+    def kill(self): 
+        self.server_sock.close()
+        self.set_discoverable(False)
+        sys.exit()
 
 if __name__ == "__main__":
     sys.excepthook = _ExceptionHandler
-    signal.signal(signal.SIGINT, _SignalHandler)
-    signal.signal(signal.SIGTERM, _SignalHandler)
     logger = logging.getLogger("logger")
     handler = logging.StreamHandler(sys.stdout)
     formatter = logging.Formatter("%(asctime)s: %(message)s")
     logger.addHandler(handler)
     logger.setLevel(logging.DEBUG)
     logger.info("Debug logs enabled")
-    server = BluetoothServer()
-    _closables.append(server)
-    server.run()
+    try:
+        multithreaded_server = Server()
+        multithreaded_server.run()
+    except KeyboardInterrupt:
+        self._logger.info("shutting down the server")
+        multithreaded_server.kill()
