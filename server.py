1003a73e7a15f8d5f49925323401c94302b64df7
ComputerOnFire <computeronfire@protonmail.com>
2020-06-29 16:32:07 -0500

    silly mistake

--- server.py
+++ server.py
@@ -19,7 +19,7 @@ def _ExceptionHandler(exc_type, exc_value, exc_traceback):
 
 def hashServerFile():
     f = open(sys.argv[0],"r",encoding='utf-8')
-    serverHash = hashlib.sha256(f.read()).encode('utf-8').hexdigest()
+    serverHash = hashlib.sha256(f.read().encode('utf-8')).hexdigest()
     return serverHash
 #This is what gets spawned by the server when it receives a connection.
 # based on. Thanks. https://github.com/michaelgheith/actopy/blob/master/LICENSE.txt
